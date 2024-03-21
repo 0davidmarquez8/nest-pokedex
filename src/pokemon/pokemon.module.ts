28ca338684d9e3b2aaf635d2e983660008f17ffd
0davidmarquez8 <davidmarquezing@gmail.com>
2024-03-20 23:08:01 -0500

    Inserción múltiple con la semilla

--- src/pokemon/pokemon.module.ts
+++ src/pokemon/pokemon.module.ts
@@ -14,6 +14,7 @@ import { PokemonController } from './pokemon.controller';
         schema: PokemonSchema,
       }
     ])
-  ]
+  ],
+  exports: [MongooseModule]
 })
 export class PokemonModule {}
